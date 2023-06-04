bba4b52f98b4900d724f580159d9b9a668b67c75
HoangAnh <hoanganhgmd@gmail.com>
2023-06-04 12:56:48 +0700

    + add jump

--- Assets/Scrtips/MoveByKey.cs
+++ Assets/Scrtips/MoveByKey.cs
@@ -6,14 +6,38 @@ public class MoveByKey : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
     public float movingSpeed;
+    public float speed = 6.0f;
+    public float jumpSpeed = 20.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
 
     private void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        float vInput = Input.GetAxis("Vertical");
+        // characterController.SimpleMove(direction * movingSpeed);
 
-        Vector3 direction = transform.right * hInput + transform.forward * vInput;
-        characterController.SimpleMove(direction * movingSpeed);
+        if (characterController.isGrounded)
+        {
+            float hInput = Input.GetAxis("Horizontal");
+            float vInput = Input.GetAxis("Vertical");
+
+            moveDirection = transform.right * hInput + transform.forward * vInput;
+            // We are grounded, so recalculate move direction based on axes
+            // moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 
 }
